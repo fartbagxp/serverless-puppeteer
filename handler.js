8abd627fd9d56116b3ff16a78a878d55fb2ab5cd
Boris Ning <fartbagxp@gmail.com>
2019-09-10 03:19:25 -0400

    Got a working lambda with puppeteer

---
+++ handler.js
@@ -0,0 +1,38 @@
+"use strict";
+const puppeteer = require("puppeteer-core");
+
+module.exports.index = async (event, context) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: "/opt/headless_shell",
+      args: ["--no-sandbox", "--disable-gpu", "--single-process"]
+    });
+
+    const page = await browser.newPage();
+    await page.goto(event["queryStringParameters"].address, {
+      waitUntil: ["domcontentloaded", "networkidle0"]
+    });
+
+    const image = await page.screenshot({
+      clip: { x: 0, y: 0, width: 1024, height: 800 },
+      encoding: "base64"
+    });
+
+    browser.close();
+
+    return {
+      statusCode: 200,
+      body: image,
+      headers: {
+        "Content-Type": "image/png"
+      },
+      isBase64Encoded: true
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500
+    };
+  }
+};
